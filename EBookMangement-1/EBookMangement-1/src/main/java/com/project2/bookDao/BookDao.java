719c709a79510b4ad215b2b96bb5b5cd6cfa82d2
AbhayDevv <abhayaman2005@gmail.com>
2024-07-10 09:44:30 +0530

    added bookwebapp

--- EBookMangement-1/EBookMangement-1/src/main/java/com/project2/bookDao/BookDao.java
+++
@@ -1,9 +0,0 @@
-package com.project2.bookDao;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.project2.book.Book;
-
-public interface BookDao extends JpaRepository<Book, Integer> {
-
-}
